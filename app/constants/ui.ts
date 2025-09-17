cf9258af0c7602b7c9be178b1db2b0800ae8ee80
Jan Žunec <83420643+Janzunec@users.noreply.github.com>
2025-09-18 00:44:41 +0200

    Update ui.ts

--- app/constants/ui.ts
+++ app/constants/ui.ts
@@ -21,20 +21,13 @@ export const testimonials = [
 	},
 	{
 		id: 2,
-		quote: 'Developer of the highest calibre. We really enjoyed working with him, as he is a hard working, skilled, innovative and creative developer. Projects have been delivered flawlessly and even ahead of schedule. Great developer!',
-		name: 'Brantly Millegan - Founder & CEO',
-		title: 'EthID Org',
-		website: 'https://ethid.org',
-	},
-	{
-		id: 3,
 		quote: 'Jan is one of the most diligent, hard working devs I encountered in the web3 ecosystem. Always open to tackling new challenges, and highly responsive and adaptable. I was seriously impressed with how quickly he managed to learn the nuances of our stack, to integrate with various other protocols as well as our complex backend. It was always a pleasure to work with him.',
 		name: 'MevBadnit - Co-founder',
 		title: 'Kodex Solutions',
 		website: 'https://kodex.io',
 	},
 	{
-		id: 4,
+		id: 3,
 		quote: 'It was a pleasure working with Jan. He managed the whole project from start to the finish line, to deliver a quality product very quickly. He has respected our wishes and ideas, and he even improved them. If we ever need another digital product, we are definitely going with him. Highly recommend!',
 		name: 'Branko Žunec - Founder & CEO',
 		title: 'BMC International',
